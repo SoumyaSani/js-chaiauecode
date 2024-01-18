53292f1cddf5e9f2d26ced77ad1ab20d521e96ab
SoumyaSani <96862633+SoumyaSani@users.noreply.github.com>
2024-01-18 05:30:41 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,29 @@
+
+function one(){
+
+    const username = "hitesh"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+
+    }
+   // console.log(website);
+
+    two()
+}
+
+one();
+
+console.log(addOne(5));
+function addOne(num)
+{
+    return num + 1;
+}
+
+//console.log(addTwo(5));
+const addTwo = function(num)
+{
+    return num + 2;
+}
+
+console.log(addTwo(5));
