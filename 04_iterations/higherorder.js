37b4dad7fbf84bfe01259105ea3fb30f829ef30f
SoumyaSani <96862633+SoumyaSani@users.noreply.github.com>
2024-01-22 05:48:59 +0000

    loop

---
+++ 04_iterations/higherorder.js
@@ -0,0 +1,63 @@
+// for of
+
+// const arr =[1,2,3,4,5]
+
+// for (const num of arr)
+// {
+//     console.log(num);
+// }
+
+// const greetings ="Hello world";
+// for (const greet of greetings) {
+//     console.log(greet);
+// }
+
+//Maps
+
+const map = new Map();
+map.set('IN', "India");
+map.set('USA', "United State Of America")
+map.set('Fr', "France");
+
+//console.log(map);
+
+// for (const key of map) {
+//     console.log(key);
+    
+// }
+
+// for (const [key,value] of map) {
+//     console.log(key, ":-", value);
+// }
+
+// const myobj = {
+//     'game1' : 'NFS',
+//     'game2' : 'spiderman'
+// }
+
+// for (const [key] of myobj) {
+//     console.log(key);
+// }
+
+
+
+// const myObject = {
+//     js : "JavaScript",
+//     cpp : "c++",
+//     rb : "ruby",
+//     swift : "swift by apple"
+// }
+
+// for (const key in myObject) {
+//    console.log(`${key} shorcut is for ${myObject[key]}`);
+// }
+
+// const programming = ["js", "java", "cpp","rb"];
+// for (const key in programming) {
+//    console.log(key);
+// }
+
+// for (const key in map) {
+//    console.log(key);
+// }
+
---
+++ 04_iterations/hoarray.js
@@ -0,0 +1,35 @@
+const coading =["js", "rb", "java", "cpp"];
+
+// coading.forEach( function (item) {
+//     console.log(item);
+// } )
+//coading.forEach( (item) => {console.log(item)} )
+
+// function printMe(item)
+// {
+//     console.log(item);
+// }
+
+// coading.forEach(printMe)
+
+// coading.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+// } )
+
+const myCoading =[
+    {
+    languageName :"javaScaript",
+    languageFileName :"js"
+},
+{
+    languageName :"java",
+    languageFileName :"java"
+},
+{
+    languageName :"python",
+    languageFileName :"py"
+}]
+
+myCoading.forEach((item) => {
+    console.log(item);
+})
