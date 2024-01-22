a4a155e7bc2cbe3d7427c6027a0eb5dbf54dd97d
SoumyaSani <96862633+SoumyaSani@users.noreply.github.com>
2024-01-22 12:01:11 +0000

    fmr

---
+++ 04_iterations/mapreducefilter.js
@@ -0,0 +1,23 @@
+// const coading = ["js", "rb", "java", "py", "cpp"]
+
+// const values = coading.forEach( (item) => {
+//     console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter( (num) => {
+//     return num > 4
+// })
+
+const newNums = []
+myNums.forEach( (num) => {
+    if(num > 4){
+        newNums.push(num)
+    }
+} )
+
+console.log(newNums);
